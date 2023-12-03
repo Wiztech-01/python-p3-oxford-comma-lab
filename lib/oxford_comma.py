5017fbe6c926e08fdb7700d4039937e3e71f4a58
Wiztech-01 <lynnnyaoke@gmail.com>
2023-12-03 18:03:55 +0300

    complete assigment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
+# oxford_comma.py
 def oxford_comma(items):
-    return None
+    #If length of array is 1, return the item. If the length is 2, return 'item 1 and item 2' else, join the items to a string
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        return ', '.join(items[:-1]) + f", and {items[-1]}"
